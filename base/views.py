5c03f58ab2a7715eb8018fb0c5b1a37633fba3b5
talama014 <talama014@gmail.com>
2021-12-18 14:54:02 +0700

    Create register

--- base/views.py
+++ base/views.py
@@ -1,4 +1,5 @@
 from django import forms
+import django
 from django.http import HttpResponse
 from django.http import request
 from django.shortcuts import redirect, render
@@ -9,11 +10,17 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 
 
 def loginPage(request):
+    page = 'login'
+
+    if request.user.is_authenticated:
+        return redirect('home')
+
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
 
         try:
@@ -28,8 +35,23 @@ def loginPage(request):
             return redirect('home')
         else:
             messages.error(request, 'Username or passwords does not exist')
-    context = {}
+    context = {'page': page}
     return render(request, 'base/login_register.html', context)
+
+def registerPage(request):
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        messages.error(request, 'Having trouble in register')    
+    return render(request, 'base/login_register.html', {'form': form})    
+
 def logoutUser(request):
     logout(request)
     return redirect('home')
@@ -51,7 +73,8 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    context = {'room': room}        
+    messages = room.message_set.all()
+    context = {'room': room, 'messages': messages}        
     return render(request, 'base/room.html', context)
 
 @login_required(login_url='/login')
