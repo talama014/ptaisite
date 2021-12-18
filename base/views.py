455802bdf051ff632180af1de7e7d06cbbc11ec6
talama014 <talama014@gmail.com>
2021-12-18 11:43:01 +0700

    Create function search

--- base/views.py
+++ base/views.py
@@ -1,15 +1,28 @@
 from django import forms
+from django.http import request
 from django.shortcuts import redirect, render
+from django.db.models import Q
 from .models import Room, Topic
 from .forms import RoomForm
 
-def home(request):
-    q = request.GET.get('q')
 
-    rooms = Room.objects.filter(topic__name=q)
+def loginPage(request):
+    context = {}
+    return render(request, 'base/login_register.html', context)
+
+def home(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+    )
     topics = Topic.objects.all()
 
-    context = {'rooms': rooms, 'topics': topics}
+    room_count = rooms.count()
+
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
